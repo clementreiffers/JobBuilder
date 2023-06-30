6dbb4795597082e3c81726b386e54fbda5609e95
clementreiffers <reiffersclement@gmail.com>
2023-06-30 15:24:42 +0200

    unitl getting dockerfile

--- controllers/job_builder.go
+++ controllers/job_builder.go
@@ -1,119 +1,69 @@
 package controllers
 
 import (
-	corev1 "k8s.io/api/core/v1"
+	batchv1 "k8s.io/api/batch/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-/*
-apiVersion: v1
-kind: Pod
-metadata:
-  name: kaniko
-spec:
-  initContainers:
-    - name: download-files
-      image: public.ecr.aws/aws-cli/aws-cli:latest
-      env:
-        - name: AWS_PROFILE
-          value: default
-        - name: AWS_ENDPOINT
-          value: https://s3.fr-par.scw.cloud
-        - name: AWS_BUCKET
-          value: stage-cf-worker
-      volumeMounts:
-        - name: s3-config
-          mountPath: /root/.aws
-          readOnly: true
-        - name: context
-          mountPath: /context
-      command:
-        - aws
-        - "--endpoint-url=$(AWS_ENDPOINT)"
-      args:
-        - s3
-        - sync
-        - "s3://$(AWS_BUCKET)"
-        - "/context"
-        - "--debug"
-  containers:
-    - name: kaniko
-      image: gcr.io/kaniko-project/executor:latest
-      args: ["--dockerfile=Dockerfile",
-             "--context=/context",
-             "--destination=clementreiffers/artist-worker:latest"]
-      volumeMounts:
-        - name: registry-credentials
-          mountPath: /kaniko/.docker/
-          readOnly: true
-        - name: context
-          mountPath: /context
-  volumes:
-    - name: registry-credentials
-      projected:
-        sources:
-          - secret:
-              name: docker-hub
-              items:
-                - key: .dockerconfigjson
-                  path: config.json
-    - name: s3-config
-      projected:
-        sources:
-          - secret:
-              name: s3-credentials
-              items:
-                - key: credentials
-                  path: credentials
-          - configMap:
-              name: aws-config
-              items:
-                - key: config
-                  path: config
-    - name: context
-      emptyDir: {}
-  restartPolicy: Never
-*/
-
-func create_job_builder() corev1.Pod {
-	env := []corev1.EnvVar{
-		{
-			Name:  "AWS_PROFILE",
-			Value: "default",
-		},
-		{
-			Name:  "AWS_ENDPOINT",
-			Value: "https://s3.fr-par.scw.cloud",
-		},
-		{
-			Name:  "AWS_BUCKET",
-			Value: "stage-cf-worker",
-		},
+func generateAwsConfig() []v1.EnvVar {
+	return []v1.EnvVar{
+		{Name: "AWS_PROFILE", Value: "default"},
+		{Name: "AWS_ENDPOINT", Value: "https://s3.fr-par.scw.cloud"},
+		{Name: "AWS_BUCKET", Value: "stage-cf-worker"},
 	}
-	volumeMounts := []corev1.VolumeMount{{Name: "s3-config", MountPath: "/root/.aws", ReadOnly: true}}
+}
 
-	return corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{Name: "kaniko"},
-		Spec: corev1.PodSpec{
-			InitContainers: []corev1.Container{
-				{
-					Name:         "download files",
-					Image:        "public.ecr.aws/aws-cli/aws-cli:latest",
-					Env:          env,
-					VolumeMounts: volumeMounts,
-					Command:      []string{"aws", "--endpoint=$(AWS_ENDPOINT)"},
-					Args:         []string{"s3", "sync", "s3://$(AWS_BUCKET)", "/context"},
-				},
-				{
-					Name:         "Generate Capnp",
-					Image:        "node",
-					Env:          env,
-					VolumeMounts: volumeMounts,
-					Command:      []string{"git", "clone", ""},
+func generateDownloadFilesContainer() v1.Container {
+	return v1.Container{
+		Name:            "download-files",
+		Image:           "public.ecr.aws/aws-cli/aws-cli:latest",
+		ImagePullPolicy: "IfNotPresent",
+		Env:             generateAwsConfig(),
+		VolumeMounts: []v1.VolumeMount{
+			{Name: "s3-config", MountPath: "/root/.aws", ReadOnly: true},
+			{Name: "context", MountPath: "/context"},
+		},
+		Command: []string{"aws", "--endpoint-url=$(AWS_ENDPOINT)"},
+		Args:    []string{"s3", "sync", "s3://$(AWS_BUCKET)", "/context"},
+	}
+}
+
+func generateGettingDockerfile() v1.Container {
+	return v1.Container{
+		Name:            "getting-dockerfile",
+		Image:           "curlimages/curl",
+		ImagePullPolicy: "IfNotPresent",
+		VolumeMounts: []v1.VolumeMount{
+			{Name: "context", MountPath: "/context", ReadOnly: false},
+		},
+		Command: []string{"curl"},
+		Args:    []string{"-o", "/context/Dockerfile", "-L", "https://raw.githubusercontent.com/clementreiffers/JobBuilder/main/build-worker.Dockerfile"},
+	}
+}
+
+func createJob() batchv1.Job {
+	ttl := int32(3600)
+	parallelism := int32(1)
+	completions := int32(1)
+	return batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "job-go",
+			Namespace: "default",
+		},
+		Spec: batchv1.JobSpec{
+			Parallelism:             &parallelism,
+			Completions:             &completions,
+			TTLSecondsAfterFinished: &ttl,
+			Template: v1.PodTemplateSpec{
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						generateDownloadFilesContainer(),
+						generateGettingDockerfile(),
+					},
 				},
 			},
-			Containers: nil,
 		},
-		Status: corev1.JobStatus{},
+		Status: batchv1.JobStatus{},
 	}
 }
