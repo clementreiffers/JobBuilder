781d313105df0438f635e7b37b172302d9ab07a0
clementreiffers <reiffersclement@gmail.com>
2023-06-30 15:49:55 +0200

    normally totally written

--- controllers/job_builder.go
+++ controllers/job_builder.go
@@ -42,28 +42,120 @@ func generateGettingDockerfile() v1.Container {
 	}
 }
 
+func generateCapnp() v1.Container {
+	return v1.Container{
+		Name:            "generating-capnp",
+		Image:           "node",
+		ImagePullPolicy: "IfNotPresent",
+		Env:             generateAwsConfig(),
+		VolumeMounts: []v1.VolumeMount{
+			{Name: "s3-config", MountPath: "/root/.aws", ReadOnly: true},
+			{Name: "context", MountPath: "/context"},
+		},
+		Command: []string{"npx", "new-capnp-generator"},
+		Args: []string{
+			"--bucketName=$(AWS_BUCKET)",
+			"--s3Endpoint=$(AWS_ENDPOINT)",
+			"--outFile=/context/config.capnp",
+		},
+	}
+}
+
+func generateKaniko() v1.Container {
+	return v1.Container{
+		Name:  "kaniko",
+		Image: "gcr.io/kaniko-project/executor:latest",
+		Args: []string{
+			"--dockerfile=Dockerfile",
+			"--context=/context",
+			"--destination=clementreiffers/artist-worker:latest",
+		},
+		VolumeMounts: []v1.VolumeMount{
+			{Name: "registry-credentials", MountPath: "/kaniko/.docker/", ReadOnly: true},
+			{Name: "context", MountPath: "/context"},
+		},
+	}
+}
+
+func generateVolumes() []v1.Volume {
+	return []v1.Volume{
+		{
+			Name: "registry-credentials",
+			VolumeSource: v1.VolumeSource{
+				Projected: &v1.ProjectedVolumeSource{
+					Sources: []v1.VolumeProjection{
+						{
+							Secret: &v1.SecretProjection{
+								LocalObjectReference: v1.LocalObjectReference{
+									Name: "docker-hub",
+								},
+								Items: []v1.KeyToPath{
+									{Key: ".dockerconfigjson", Path: "config.json"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			Name: "s3-config",
+			VolumeSource: v1.VolumeSource{
+				Projected: &v1.ProjectedVolumeSource{
+					Sources: []v1.VolumeProjection{
+						{
+							Secret: &v1.SecretProjection{
+								LocalObjectReference: v1.LocalObjectReference{Name: "s3-credentials"},
+								Items: []v1.KeyToPath{
+									{Key: "credentials", Path: "credentials"},
+								},
+							},
+						},
+						{
+							ConfigMap: &v1.ConfigMapProjection{
+								LocalObjectReference: v1.LocalObjectReference{Name: "aws-config"},
+								Items: []v1.KeyToPath{
+									{Key: "config", Path: "config"},
+								},
+								Optional: nil,
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			Name: "context",
+			VolumeSource: v1.VolumeSource{
+				EmptyDir: &v1.EmptyDirVolumeSource{},
+			},
+		},
+	}
+}
+
 func createJob() batchv1.Job {
 	ttl := int32(3600)
-	parallelism := int32(1)
-	completions := int32(1)
 	return batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "job-go",
 			Namespace: "default",
 		},
 		Spec: batchv1.JobSpec{
-			Parallelism:             &parallelism,
-			Completions:             &completions,
+			Parallelism:             new(int32),
+			Completions:             new(int32),
 			TTLSecondsAfterFinished: &ttl,
 			Template: v1.PodTemplateSpec{
 				Spec: v1.PodSpec{
 					Containers: []v1.Container{
 						generateDownloadFilesContainer(),
 						generateGettingDockerfile(),
+						generateCapnp(),
+						generateKaniko(),
 					},
+					Volumes:       generateVolumes(),
+					RestartPolicy: "Never",
 				},
 			},
 		},
-		Status: batchv1.JobStatus{},
 	}
 }
